73f94c00f7c5a023d2b5fe3604ce3ed6a09e1295
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2023-08-26 16:15:04 -0400

    Refatorando criação de Video

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -26,35 +26,6 @@ export class RoomController {
 
   }
 
-  async createVideo(req: Request, res: Response) {
-    const { title, url } = req.body
-    const { idRoom } = req.params
-
-    try {
-
-      const room = await roomRepository.findOneBy({ id: Number(idRoom) })
-
-      if (!room) {
-        return res.status(404).json({ message: 'A aula não existe' })
-      }
-
-      const newVideo = videoRepository.create({
-        title,
-        url,
-        room
-      })
-      await videoRepository.save(newVideo)
-      return res.status(201).json(newVideo)
-
-    } catch (error) {
-
-      console.log(error)
-      return res.status(500).json({ message: 'Internal server error' })
-
-    }
-
-  }
-
   async roomSubject(req: Request, res: Response) {
     const { subject_id } = req.body
     const { idRoom } = req.params
